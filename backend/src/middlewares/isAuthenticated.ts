95bb0726ff029b6c84f05c7e73986f7ff9209547
leonardohernandesq <leonardo_hernandes@outlook.com.br>
2023-05-25 00:31:44 +0100

    build backnd

--- backend/src/middlewares/isAuthenticated.ts
+++ backend/src/middlewares/isAuthenticated.ts
@@ -1,4 +1,4 @@
-import {Request, Response, NextFunction, response} from 'express';
+import {Request, Response, NextFunction} from 'express';
 import { verify } from 'jsonwebtoken';
 
 interface IPayLoad{
